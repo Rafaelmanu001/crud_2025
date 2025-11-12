93f4ece190084b8b765491454789176776b7cf09
Rafael Fragoso <72579993+Rafaelmanu001@users.noreply.github.com>
2025-11-11 21:01:05 -0300

    Some editions about my school "Project"

---
+++ src/main/java/model/bean/TipoProduto.java
@@ -0,0 +1,36 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model.bean;
+
+/**
+ *
+ * @author Rafael
+ */
+public class TipoProduto {
+    private int id;
+    private String descricao;
+    
+// Metodos de encapsulamento
+public int getId(){
+    return id;
+    
+}    
+public void setId(int id) {
+    this.id = id;
+    
+}
+public String getDescricao() {
+    return descricao;
+            
+}
+public void setDescricao(String descricao) {
+    this.descricao = descricao;
+    
+}
+public String toString() {
+    return descricao;
+    
+}
+}
